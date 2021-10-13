fcc3f99288d0a4999f9af10aaf0c9b1b14a6411a
pechenkokirill <pechenkokirill2001@gmail.com>
2021-10-13 19:34:16 +0300

    Создание проекта

---
+++ PizzaCore.Front/src/App.vue
@@ -0,0 +1,82 @@
+<template>
+  <router-view/>
+</template>
+
+<script>
+
+
+export default {
+  components: {
+  }
+}
+</script>
+
+<style>
+
+@font-face {
+  font-family: 'Meter';
+  src: url(../public/fonts/Maler.ttf);
+}
+
+.text-white
+{
+  color: white;
+}
+
+.container
+{
+    margin-left: auto;
+    margin-right: auto;
+    max-width: 1366px;
+    padding-left: 32px;
+    padding-right: 32px;
+}
+
+.btn
+{
+   border-radius: 5px;
+   border: 0;
+   padding: 8px 16px;
+   cursor: pointer;
+   transition: opacity 250ms;
+}
+
+.btn:hover
+{
+  opacity: 0.8;
+}
+
+a
+{
+   text-decoration: none;
+}
+
+body
+{
+   background-repeat: repeat;
+   background-image: url(../public/img/back.jpg);
+   background-size: 256px;
+   margin: 0;
+}
+
+.flex
+{
+  display: flex;
+}
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
