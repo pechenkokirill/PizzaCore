1b5a0413792e145e86993ac25adc67bf163d76d1
pechenkokirill <pechenkokirill2001@gmail.com>
2021-10-13 19:42:30 +0300

    Добавление карусели на странице Home.vue

---
+++ PizzaCore.Front/src/components/BillboardCarousel.vue
@@ -0,0 +1,66 @@
+<template>
+  <carousel
+    id="billboard"
+    :autoplay="3000"
+    :wrapAround="true"
+    :settings="settings"
+    :breakpoints="breakpoints"
+    v-model="currentSlide"
+  >
+    <slide v-for="slide in 10" :key="slide">
+      <div class="carousel__item">
+        <img src="../../public/img/carusel1.png" style="width: 100%" alt="" />
+      </div>
+    </slide>
+  </carousel>
+</template>
+
+<script>
+import { Carousel, Slide } from "vue3-carousel";
+import "vue3-carousel/dist/carousel.css";
+
+export default {
+  name: "BillboardCarousel",
+  mounted() {
+    this.currentSlide = 0;
+  },
+  data: () => ({
+    breakpoints: {
+      1366: {
+        itemsToShow: 1.5,
+      },
+    },
+    settings: {
+      itemsToShow: 1,
+    },
+    currentSlide: undefined,
+  }),
+  components: {
+    Carousel,
+    Slide,
+  },
+};
+</script>
+
+<style scoped>
+#billboard .carousel {
+  margin-top: 32px;
+}
+
+#billboard .carousel__slide {
+  padding: 48px;
+}
+
+#billboard .carousel__item > img {
+  border-radius: 10px;
+  box-shadow: 0 4px 10px rgb(63 93 138 / 10%);
+}
+
+.carousel__item {
+  transition: 0.5s;
+}
+
+.carousel__slide--active > .carousel__item {
+  transform: scale(1.1);
+}
+</style>
--- PizzaCore.Front/src/views/Home.vue
+++ PizzaCore.Front/src/views/Home.vue
@@ -1,13 +1,16 @@
 <template>
   <div>
+    <billboard-carousel/>
   </div>
 </template>
 
 <script>
+import BillboardCarousel from '../components/BillboardCarousel.vue';
 
 export default {
   name: "Home",
   components: {
+    BillboardCarousel
   },
 };
 </script>
